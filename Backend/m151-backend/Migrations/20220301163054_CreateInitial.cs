5b22c48b4f3734996a48a2d4b64c3c26e652b6d0
Eric Wermelinger <eric.wermelinger@diartis.ch>
2022-03-01 17:52:25 +0100

    Create Project & Repository #01

---
+++ Backend/m151-backend/Migrations/20220301163054_CreateInitial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace m151_backend.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
