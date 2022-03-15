7b643deea3f88a9b04a242c29683fe9515b528fe
Eric Wermelinger <eric.wermelinger@diartis.ch>
2022-03-15 14:05:11 +0100

    Add Configuration #06

---
+++ Frontend/M151/src/app/Config/appRoutes.ts
@@ -0,0 +1,10 @@
+export const appRoutes = {
+    Empty: '',
+    AllRuns: 'allRuns',
+    GpxFile: 'gpxFile',
+    Login: 'login',
+    MyRuns: 'myRuns',
+    Notes: 'notes',
+    Refresh: 'refresh',
+    UserData: 'userData'
+}
